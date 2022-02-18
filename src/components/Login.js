3875d43b4add653ecb68c83e42b469922bf41ee0
FabianOcampo91 <faby.ocampo91@gmail.com>
2022-02-18 19:48:15 -0300

    M3 login react redux

---
+++ src/components/Login.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { login } from '../features/userSlice'
+import './Login.css'
+
+
+const Login = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const dispatch = useDispatch()
+  
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(
+      login({
+        name: name,
+        email: email,
+        password: password,
+        loggedIn: true,
+      })
+    );
+  }
+
+  return (
+    <div className='login'>
+      <form className='login-form' onSubmit={(e) => handlerSubmit(e)}>
+        <h1>Login Here</h1>
+        <input 
+          type="name" 
+          placeholder='Name' 
+          value={name} 
+          onChange={(e) => setName(e.target.value)} />
+        <input 
+          type="email" 
+          placeholder='Email' 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} />
+        <input 
+          type="password" 
+          placeholder='Password' 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} />
+        <button 
+          type="submit" 
+          className='submit-btn'>
+            Enviar
+          </button>
+
+
+
+      </form>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/Logout.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { logout, selectUser } from '../features/userSlice';
+import './Logout.css'
+
+const Logout = () => {
+  const user = useSelector(selectUser);
+
+
+  const dispatch = useDispatch();
+  const handlerLogout = (e) => {
+    e.preventDefault ();
+    dispatch(logout())
+  }
+
+
+  return (
+    <div className='logout'>
+        <h1>
+          Welcome <span className='user-name'>Fabian</span>
+        </h1>
+          <button className='logout-btn' onClick={(e) => handlerLogout(e)}>Logout</button>
+    </div>
+  )
+}
+
+export default Logout
